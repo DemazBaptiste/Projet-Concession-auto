ed455c73fd27169e8fdb0aebad0218345a83c070
unknown <antoinelemay@cfai63.fr>
2020-12-16 11:04:57 +0100

    saisie

--- Source/saisie.cpp
+++ Source/saisie.cpp
@@ -4,4 +4,20 @@ do{
 		clear_buffer();
 		cout << "\nNom joueur 1 :";
 		getline(cin >> ws, j1);
-	}while(cin.fail() || j1.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ") != string::npos);*/
+	}while(cin.fail() || j1.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ") != string::npos);
+	
+
+void clear_buffer()
+{
+    cin.clear();
+    cin.seekg(0, ios::end);
+
+    if(!cin.fail())
+    {
+        cin.ignore(numeric_limits<streamsize>::max()); // Le flux a déjà un état valide donc inutile de faire appel à clear()
+    }
+    else
+    {
+        cin.clear(); // Le flux est dans un état invalide donc on le remet en état valide
+    }
+}*/
