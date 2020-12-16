b56d6d1b13cbc27f6a01f22af14f19e3c4438b72
Julien Jacques <julien.jacques63260@gmail.com>
2020-12-16 11:51:09 +0100

    add main

--- main.cpp
+++ main.cpp
@@ -3,15 +3,35 @@
 #include <string>
 using namespace std;
 
+void clear_buffer()
+    {
+    cin.clear();
+    cin.seekg(0, ios::end);
 
+    if(!cin.fail())
+     {
+        cin.ignore(numeric_limits<streamsize>::max()); // Le flux a déjà un état valide donc inutile de faire appel à clear()
+     }
+    else
+    {
+        cin.clear(); // Le flux est dans un état invalide donc on le remet en état valide
+    }
+}
 
 int Sous_menu()
 {
   int choiceMenu2; //variable stockant le choix sous menu
 
-    std::cout<<"\n-----SOUS-MENU-----\n"<<"Choice 1 : Modifier\n"<<"Choice 2 : Ajouter \n"<<"Choice 3 : Supprimer \n"<<"Choice 4 : Liste\n"<<"Votre choix ?\n";
+    std::cout<<"\n-----SOUS-MENU-----\n"<<"Choice 1 : Modifier\n"<<"Choice 2 : Ajouter \n"<<"Choice 3 : Supprimer \n"<<"Choice 4 : Liste\n"<<"\n";
 
-    cin>>choiceMenu2;
+    string CHOIX;
+
+    do{
+		  clear_buffer();
+		  cout << "\n VOTRE CHOIX ? :";
+		  getline(cin >> ws, CHOIX);
+	  }while(cin.fail() || CHOIX.find_first_not_of("1234") != string::npos);
+      choiceMenu2=stoi(CHOIX);//transforme choix de string vers int
     switch(choiceMenu2)
     {
       case 1 :
@@ -41,10 +61,17 @@ int Sous_menu()
 int Menu1()
 {
   int choiceMenu;
-      std::cout<<"\n-----MENU-----\n"<<"Choice 1 : stock\n"<<"Choice 2 : Ventes \n"<<"Choice 3 : Employés \n"<<"Choice 4 : réparations\n"<<"Choice 5 : Clients\n"<<"Votre choix ?\n";
+      std::cout<<"\n-----MENU-----\n"<<"Choice 1 : stock\n"<<"Choice 2 : Ventes \n"<<"Choice 3 : Employés \n"<<"Choice 4 : réparations\n"<<"Choice 5 : Clients\n"<<"\n";
 
 
-      cin>>choiceMenu;
+      string CHOIX;
+
+    do{
+		  clear_buffer();
+		  cout << "\n VOTRE CHOIX ? :";
+		  getline(cin >> ws, CHOIX);
+	  }while(cin.fail() || CHOIX.find_first_not_of("12345") != string::npos);
+      choiceMenu=stoi(CHOIX);//transforme choix de string vers int
     switch(choiceMenu)
     {
       case 1 :
