d66642ff746cb158fcb667f7b077c637d1711a63
unknown <antoinelemay@cfai63.fr>
2020-12-17 18:36:21 +0100

    J'avais oublié Mechanic.cpp

---
+++ Source/Mechanic.cpp
@@ -0,0 +1,13 @@
+#include "../lib/Mechanic.hpp"
+#include"../lib/Employee.hpp"
+
+Mechanic::Mechanic(){
+  Employee();
+  //TODO set mechanic idEmployee
+}
+void Mechanic::repair(string idCustomer, string idCar){
+  //TODO create corresponding Call_out;
+}
+void Mechanic::write_syntax(){
+  //TODO
+}
