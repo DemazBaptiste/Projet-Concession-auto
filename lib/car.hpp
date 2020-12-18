cd3be5d618c0993a8ed229a598c1f8ea0451d097
Baptiste Demazeau <bdemazeau.pro@gmail.com>
2020-12-18 09:56:50 +0100

    Adding car class complete

--- lib/car.hpp
+++ lib/car.hpp
@@ -12,29 +12,16 @@ class Car
     string id;
 
   public:
-<<<<<<< HEAD
     Car();
 		~Car();
     Car(string ,string );
     void setbrand(string brand);
-=======
-    car();
-		~car();
-    car(string ,string );
-    void setbrand(string _brand);
->>>>>>> d64569cf3874f86bb392288d4ccb1ee2cad2110f
 		string getbrand();
 		void setmodel(string _brand);
 		string getmodel();
     void setid(string name);
     string getid();
-<<<<<<< HEAD
-		void setprice(double price);
-    string getprice();
-		void printCar();
-=======
 		void setprice(float _price);
     float getprice();
-		void printcar();
->>>>>>> d64569cf3874f86bb392288d4ccb1ee2cad2110f
+		void printCar();
 };
