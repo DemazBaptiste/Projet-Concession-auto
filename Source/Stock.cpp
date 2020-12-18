5f5f628111eb5b409373da41e2ccb6afe21de382
Baptiste Demazeau <bdemazeau.pro@gmail.com>
2020-12-18 10:28:34 +0100

    Adding Stock.cpp Functions

---
+++ Source/Stock.cpp
@@ -0,0 +1,32 @@
+#include "Stock.hpp"
+
+
+Stock::Stock()
+{
+
+}
+
+Stock::~Stock()
+{
+
+}
+
+void Stock::remove_car(string Car)
+{
+    list.pop_back(car);
+}
+
+void Stock::add_car(string Car)
+{
+    list.push.back(car);
+}
+
+void Stock::getByID()
+{
+    
+}
+
+void Stock::print_Stock()
+{
+     cout << Car.list << endl;
+}
--- lib/Stock.hpp
+++ lib/Stock.hpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "Car.hpp"
 
 using namespace std;
 
-class stock
+class Stock
 {
     private:
-     string car;
+     string Car;
     
     public:
     Stock();
     ~Stock();
+    void remove_car(string Car);
+    void add_car(string Car);
+    void getByID();
+    void print_Stock();
+    vector<Car> list;
 };
