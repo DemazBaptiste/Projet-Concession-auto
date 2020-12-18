76c10d7ed93d1ddcbfc8bf0371cbd7f1db294d59
unknown <antoinelemay@cfai63.fr>
2020-12-18 14:02:29 +0100

    write syntax + Mechanic id

--- Source/Mechanic.cpp
+++ Source/Mechanic.cpp
@@ -1,13 +1,15 @@
 #include "../lib/Mechanic.hpp"
 #include"../lib/Employee.hpp"
+#include <string>
 
-Mechanic::Mechanic(){
-  Employee();
-  //TODO set mechanic idEmployee
+Mechanic::Mechanic(string f, string l , string d){
+  string q = "m";
+  Employee(f, l, d, q);
 }
 void Mechanic::repair(string idCustomer, string idCar){
   //TODO create corresponding Call_out;
 }
 void Mechanic::write_syntax(){
-  //TODO
+  Human::write_syntax();
+  cout << "ID Garagiste : " << idEmployee << "\n";
 }
--- Source/Seller.cpp
+++ Source/Seller.cpp
@@ -6,9 +6,11 @@ Seller::Seller(string f, string l, string d){
   string q = "s";
   Employee(f, l, d, q);
 }
+
 void Seller::sell(string idCustomer, string idCar){
   //TODO create corresponding Call_out;
 }
 void Seller::write_syntax(){
-  //TODO
+  Human::write_syntax();
+  cout << "ID Vendeur : " << idEmployee << "\n";
 }
